b47be8e57255c4eb5fe442bb852b5ea8351cba6a
lemondy <lemondy9@gmail.com>
2016-01-10 09:57:08 +0800

    修改BalanceTree文件,该文件是判断给定二叉树是否是高度平衡的.

--- src/cn/edu/bit/lemondy/leetcode/easy/BalanceTree.java
+++ src/cn/edu/bit/lemondy/leetcode/easy/BalanceTree.java
@@ -2,17 +2,30 @@ package cn.edu.bit.lemondy.leetcode.easy;
 
 import java.util.LinkedList;
 
+/**
+ * 
+ * @author lemon
+ * problem: 
+ * 			给定一颗二叉树，判断该树是否是高度平衡的。高度平衡指的是对每个节点它的左右子树高度差不超过1.
+ * solution： 
+ * 			基于求树深度算法，遍历二叉树的节点，依次判断每个节点是否满足要求。
+ * 
+ */
+
 public class BalanceTree {
 
 	public class TreeNode {
-		      int val;
-		      TreeNode left;
-		      TreeNode right;
-		      TreeNode(int x) { val = x; }
-		  }
+		  int val;
+		  TreeNode left;
+		  TreeNode right;
+		  TreeNode(int x) { val = x; }
+		  public TreeNode() {
+			// TODO Auto-generated constructor stub
+		}
+	}
 		 
 	
-	 public int treeHeight(TreeNode node){
+	public int treeHeight(TreeNode node){
 	        int hl,hr,max;
 	        if(node != null){
 	            hl = treeHeight(node.left);
@@ -25,15 +38,15 @@ public class BalanceTree {
 	    }
 	    
 	    
-	    public boolean isBalanced(TreeNode root) {
+	public boolean isBalanced(TreeNode root) {
 	    	if(root == null)
 	    		return true;
-	        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+	        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();  //先入先出队列
 	        queue.addLast(root);
 	        int hl=0,hr=0;
 	        
 	        while(!queue.isEmpty()){
-	            TreeNode node = (TreeNode)queue.removeFirst();
+	            TreeNode node = (TreeNode)queue.removeFirst();   //判断当前节点是否是高度平衡
 	            hl = treeHeight(node.left);
 	            hr = treeHeight(node.right);
 	            if(Math.abs(hl-hr)>1)
@@ -51,7 +64,7 @@ public class BalanceTree {
 	        
 	    }
 	    
-	    public static void main(String[] args) {
+	public static void main(String[] args) {
 			
-		}
+	}
 }
